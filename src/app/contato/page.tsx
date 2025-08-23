409a53af8af2127cb641e463c0060c152d2eed8e
Thiago <thiagodossantos315@gmail.com>
2025-08-23 11:40:36 -0300

    feat: add social media icons and links to Contato component

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,4 +1,6 @@
 import { Github, Linkedin, Mail, Twitter } from "lucide-react"
+import { FaTiktok, FaInstagram, FaFacebook } from "react-icons/fa" // Importing icons from react-icons
+import { FaThreads } from "react-icons/fa6";
 
 export default function Contato() {
   const contactLinks = [
@@ -6,6 +8,10 @@ export default function Contato() {
     { icon: Linkedin, label: "LinkedIn", href: "https://linkedin.com/in/seu-perfil" },
     { icon: Github, label: "GitHub", href: "https://github.com/IamThiago-IT" },
     { icon: Twitter, label: "Twitter", href: "https://twitter.com/IamThiago-IT" },
+    { icon: FaTiktok, label: "TikTok", href: "https://www.tiktok.com/@IamThiago-IT" },
+    { icon: FaInstagram, label: "Instagram", href: "https://www.instagram.com/IamThiago-IT" },
+    { icon: FaFacebook, label: "Facebook", href: "https://www.facebook.com/IamThiago-IT" },
+    { icon: FaThreads, label: "Threads", href: "https://www.threads.net/@IamThiago-IT" },
   ]
 
   return (
@@ -18,34 +24,54 @@ export default function Contato() {
       <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
         {contactLinks.map((link) => {
           // Define brand color classes
-          let hoverText = "";
-          let hoverIcon = "";
-          let hoverBorder = "";
+          let hoverText = ""
+          let hoverIcon = ""
+          let hoverBorder = ""
           switch (link.label) {
             case "GitHub":
-              hoverText = "hover:text-[#181717]"; // GitHub black
-              hoverIcon = "group-hover:text-[#181717]";
-              hoverBorder = "hover:border-[#181717]";
-              break;
+              hoverText = "hover:text-[#181717]" // GitHub black
+              hoverIcon = "group-hover:text-[#181717]"
+              hoverBorder = "hover:border-[#181717]"
+              break
             case "Twitter":
-              hoverText = "hover:text-[#1DA1F2]"; // Twitter blue
-              hoverIcon = "group-hover:text-[#1DA1F2]";
-              hoverBorder = "hover:border-[#1DA1F2]";
-              break;
+              hoverText = "hover:text-[#1DA1F2]" // Twitter blue
+              hoverIcon = "group-hover:text-[#1DA1F2]"
+              hoverBorder = "hover:border-[#1DA1F2]"
+              break
             case "LinkedIn":
-              hoverText = "hover:text-[#0077B5]"; // LinkedIn blue
-              hoverIcon = "group-hover:text-[#0077B5]";
-              hoverBorder = "hover:border-[#0077B5]";
-              break;
+              hoverText = "hover:text-[#0077B5]" // LinkedIn blue
+              hoverIcon = "group-hover:text-[#0077B5]"
+              hoverBorder = "hover:border-[#0077B5]"
+              break
             case "Email":
-              hoverText = "hover:text-[#EA4335]"; // Gmail red
-              hoverIcon = "group-hover:text-[#EA4335]";
-              hoverBorder = "hover:border-[#EA4335]";
-              break;
+              hoverText = "hover:text-[#EA4335]" // Gmail red
+              hoverIcon = "group-hover:text-[#EA4335]"
+              hoverBorder = "hover:border-[#EA4335]"
+              break
+            case "TikTok":
+              hoverText = "hover:text-[#000000]" // TikTok black
+              hoverIcon = "group-hover:text-[#000000]"
+              hoverBorder = "hover:border-[#000000]"
+              break
+            case "Threads":
+              hoverText = "hover:text-[#000000]" // Threads black
+              hoverIcon = "group-hover:text-[#000000]"
+              hoverBorder = "hover:border-[#000000]"
+              break
+            case "Instagram":
+              hoverText = "hover:text-[#E4405F]" // Instagram pink
+              hoverIcon = "group-hover:text-[#E4405F]"
+              hoverBorder = "hover:border-[#E4405F]"
+              break
+            case "Facebook":
+              hoverText = "hover:text-[#1877F2]" // Facebook blue
+              hoverIcon = "group-hover:text-[#1877F2]"
+              hoverBorder = "hover:border-[#1877F2]"
+              break
             default:
-              hoverText = "hover:text-blue-700";
-              hoverIcon = "group-hover:text-blue-700";
-              hoverBorder = "hover:border-blue-700";
+              hoverText = "hover:text-blue-700"
+              hoverIcon = "group-hover:text-blue-700"
+              hoverBorder = "hover:border-blue-700"
           }
           return (
             <a
@@ -58,7 +84,7 @@ export default function Contato() {
               <link.icon className={`w-10 h-10 mb-2 transition-colors ${hoverIcon}`} />
               <span className="font-semibold text-lg">{link.label}</span>
             </a>
-          );
+          )
         })}
       </div>
     </div>
