51c60559171dad45caac29b7bab6f47b03722678
Thiago <thiagodossantos315@gmail.com>
2025-10-05 23:28:00 -0300

    feat: refactor subscribeNewsletter function to use zod for email validation

--- src/app/Actions/newsletterActions.ts
+++ src/app/Actions/newsletterActions.ts
@@ -1,13 +1,18 @@
 "use server";
 import { sendEmail } from "./emailActions";
+import { z } from "zod";
+
+const subscribeNewsletterSchema = z.object({
+  email: z.string().email(),
+});
 
 export async function subscribeNewsletter(data: FormData): Promise<void> {
-  const email = data.get("email")?.toString();
+  const formObj = Object.fromEntries(data.entries());
+  const { email } = subscribeNewsletterSchema.parse(formObj);
+
   // ...lógica para salvar o e-mail...
   console.log("Inscrição na newsletter:", email);
-  
+
   // Envia email de confirmação (simulado)
-  if (email) {
-    await sendEmail(email, "Confirmação de Inscrição", "Obrigado por se inscrever na nossa newsletter!");
-  }
+  await sendEmail(email, "Confirmação de Inscrição", "Obrigado por se inscrever na nossa newsletter!");
 }
